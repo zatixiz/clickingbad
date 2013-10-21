1d9d7366c30a2026de68a6904c20e58dca883790
zatixiz <zatixiz@gmail.com>
2013-10-21 18:29:20 +0200

    Update and rename clickingbadcheat to clickingbadcheat.js

--- clickingbadcheat.js
+++ clickingbadcheat.js
@@ -3,7 +3,7 @@ var sv = $.parseJSON(localStorage.sv);
 pd.cash.amount = sv.cash;
 pd.widgets.amount = sv.widgets;
 pd.cash.amount = parseInt(prompt("How much money would you like?"));
-pd.widgets.amount = parseInt(promt("How many Batches would you like?"));
+pd.widgets.amount = parseInt(prompt("How many Batches would you like?"));
 
 alert("You should now have " + pd.cash.amount + " money and " + pd.widgets.amount + " batches" +
 "... Don't forget to save! (Options & Stats -> Save)");
